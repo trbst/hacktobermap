ddb9994a1fcd87c0b48244e137b661e325fb49d8
trbst <trbojevics@outlook.com>
2019-10-01 12:08:07 +0200

    Updated hacktobermap 2019 with my info

--- hacktobermap.js
+++ hacktobermap.js
@@ -5,4 +5,10 @@ var hacktobermap = [
     latitude: 20.296059,
     longitude: 85.824539
   },
+  {
+    name: "Stevan Trbojevic",
+    location: "Novi Sad",
+    latitude: 45.267136,
+    longitude: 19.833549
+  },
 ];
